36a248d2c91e237ae0398903d86cd81cae531be4
Namit Sambare <ndsambare@wustl.edu>
2020-06-29 15:57:25 -0500

    Posts now display once clicked

--- public_html/module3/notReddit/post.php
+++ public_html/module3/notReddit/post.php
@@ -5,4 +5,31 @@ require 'database.php';
 $identification = $_GET["id"];
 echo $identification;
 
+
+$stmt = $mysqli->prepare("select id, heading, link, username, time from posts where id = ? ");
+if(!$stmt){
+	printf("Query Prep Failed: %s\n", $mysqli->error);
+	exit;
+}
+$stmt->bind_param('d', $identification);
+
+$stmt->execute();
+
+$stmt->bind_result($id, $heading, $link, $username, $time);
+
+echo "<ul>\n";
+while($stmt->fetch()){
+	printf("\t<h3 %s \n %s \n %s </h3>\n",
+        htmlspecialchars($id),
+        htmlspecialchars($heading),
+        htmlspecialchars($link), 
+        htmlspecialchars($username),
+        htmlspecialchars($time)
+	);
+}
+echo "</ul>\n";
+
+
+$stmt->close();
+
 ?>
